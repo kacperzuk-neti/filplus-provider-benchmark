ca1a1cf1bc8117488c51080b862ccc051d4a38d0
Michał Mach <vipmachu@gmail.com>
2024-09-18 14:22:26 +0200

    scheduler queues connection close

--- scheduler/src/state.rs
+++ scheduler/src/state.rs
@@ -2,14 +2,15 @@ use std::sync::Arc;
 
 use crate::repository::data_repository::DataRepository;
 use rabbitmq::QueueHandler;
+use tokio::sync::Mutex;
 
 pub struct AppState {
-    pub job_queue: QueueHandler,
+    pub job_queue: Arc<Mutex<QueueHandler>>,
     pub data_repo: Arc<DataRepository>,
 }
 
 impl AppState {
-    pub fn new(job_queue: QueueHandler, data_repo: Arc<DataRepository>) -> Self {
+    pub fn new(job_queue: Arc<Mutex<QueueHandler>>, data_repo: Arc<DataRepository>) -> Self {
         AppState {
             job_queue,
             data_repo,
