7242819e0a8a8ec090f9ce5f340f68142ba6d02c
lukasz-wal <143998006+lukasz-wal@users.noreply.github.com>
2024-10-09 14:26:17 +0200

    Update entrypoint.sh
    Co-authored-by: Kacper Żuk (Neti) <117277751+kacperzuk-neti@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 METADATA=$(curl -s ${ECS_CONTAINER_METADATA_URI_V4}/task)
 
-CONTAINER_ID=$(echo $METADATA | jq -r '.Containers[0].DockerId')
+CONTAINER_ID=$(echo "$METADATA" | jq -r '.Containers[0].DockerId')
 
 export WORKER_NAME="worker-${CONTAINER_ID}"
 
